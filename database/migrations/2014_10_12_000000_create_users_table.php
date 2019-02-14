2145ab35c13e3a76f8014bfea0fbbfbc6b481a5a
aashigupta165 <aashigupta165@gmail.com>
2019-02-14 20:29:02 +0530

    adding new columns in users table and remove bugs

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,9 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('imagename')->nullable();
+            $table->string('imagesize')->nullable();
+            $table->string('email_token')->nullable();
             $table->timestamps();
         });
     }
